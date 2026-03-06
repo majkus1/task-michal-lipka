5e1515a739980c2e05dfcf252e28100b75da2c5d
majkus1 <lipkam420@gmail.com>
2026-03-06 21:35:50 +0100

    task1: add /api/task-1/people endpoint

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -5,8 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
 const server = (0, fastify_1.default)();
+const people = [
+    { name: 'Alice', age: 25, email: 'alice@example.com' },
+    { name: 'Bob', age: 30, email: 'bob@example.com' },
+    { name: 'Charlie', age: 35, email: 'charlie@example.com' },
+];
 server.get('/ping', async (request, reply) => {
     return 'pong\n';
+});
+server.get('/api/task-1/people', async () => {
+    return people;
 });
 server.listen({ port: 8080 }, (err, address) => {
     if (err) {
--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -2,8 +2,24 @@ import fastify from 'fastify'
 
 const server = fastify()
 
+type Person = {
+  name: string
+  age: number
+  email: string
+}
+
+const people: Person[] = [
+  { name: 'Alice', age: 25, email: 'alice@example.com' },
+  { name: 'Bob', age: 30, email: 'bob@example.com' },
+  { name: 'Charlie', age: 35, email: 'charlie@example.com' },
+]
+
 server.get('/ping', async (request, reply) => {
   return 'pong\n'
+})
+
+server.get('/api/task-1/people', async () => {
+  return people
 })
 
 server.listen({ port: 8080 }, (err, address) => {
